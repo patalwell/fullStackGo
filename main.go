c87c43febd7ffd7d5f6dee7a1d0d737140ae8172
Pat Alwell <pat.alwell@gmail.com>
2019-08-08 21:37:21 -0700

    experimented with queries

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 	"net/http"
 	"os"
 	"time"
+	"strconv"
 )
 
 var tpl *template.Template
@@ -38,10 +39,45 @@ func main() {
 	mux.HandleFunc("/authorization-code/callback", AuthCodeCallbackHandler)
 	mux.HandleFunc("/profile", ProfileHandler)
 	mux.HandleFunc("/logout", LogoutHandler)
+	mux.HandleFunc("/snippet/create", createSnippet)
+	mux.HandleFunc("/snippet", showSnippet)
 
 	log.Println("Starting web server on :8080")
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	// Use r.Method to check whether the request is using POST or not.
+	// If it's not, use the w.WriteHeader() method to send a 405 status code and
+	// the w.Write() method to write a "Method Not Allowed" response body. We
+	// then return from the function so that the subsequent code is not executed.
+	if r.Method != "POST" {
+		w.Header().Set("Allow", "POST")
+		http.Error(w, "Method Not Allowed", 405)
+		return
+	}
+	//Note: When dealing with JSON, you'll need to clarify that you are using JSON otherwise
+	// Go will default to plain/text
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(`{"name":"Alex"}`))
+}
+
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	// Extract the value of the id parameter from the query string and try to
+	// convert it to an integer using the strconv.Atoi() function. If it can't
+	// be converted to an integer, or the value is less than 1, we return a 404 page
+	// not found response.
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	// Use the fmt.Fprintf() function to interpolate the id value with our response // and write it to the http.ResponseWriter.
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+}
+
+
 
 func samplePage(w http.ResponseWriter, r *http.Request) {
 
@@ -67,9 +103,24 @@ func samplePage(w http.ResponseWriter, r *http.Request) {
 }
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	// Check if the current request URL path exactly matches "/".
+	// If it doesn't, use the http.NotFound() function to send
+	// a 404 response to the client.
+	// If we didn't return the handler would keep executing
+	// and return our template.
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	type customData struct {
 		Profile         map[string]string
 		IsAuthenticated bool
+	}
+
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
 	}
 
 	data := customData{
